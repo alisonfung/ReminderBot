bbf465f39ac8a2c4e17b35aa2f49b0162e2987e0
alison <tmrfandom@gmail.com>
2023-11-06 14:20:53 -0800

    converted bot to use commands extenstion

--- discord_bot.py
+++ discord_bot.py
@@ -1,63 +1,46 @@
 import discord
-import responses
+from discord.ext import commands, tasks
 import settings
+import random
+import asyncio
 
+# give discord permissions to access message content
+intents = discord.Intents.default()
+intents.message_content = True
 
-async def send_message(message, user_message, is_private):
-    # message: the message that the bot will send
-    # user_message: the message that the user sent
-    # is_private: True if the user sent a DM
-    try:
-        # get bot response depending on user message
-        response = responses.get_response(user_message)
-        # if user sent a DM, reply in DM
-        if is_private:
-            await message.author.send(response)
-        # otherwise, reply in the channel
-        else:
-            await message.channel.send(response)
-    except Exception as e:
-        print(e)
+bot = commands.Bot(command_prefix='!', intents=intents)
+# remove the library's default help command 
+bot.remove_command('help')
 
-def run_discord_bot():
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user} (ID: {bot.user.id})')
+    print('------')
 
-    # give discord bot permission to read messages 
-    intents = discord.Intents.default()
-    intents.message_content = True
-    client = discord.Client(intents=intents)
+@bot.command()
+async def hello(ctx):
+    """Says hello in response to a user."""
+    await ctx.send('Hello there!')
 
-    # on start, print in the console that the bot is running
-    @client.event
-    async def on_ready():
-        print(f'{client.user} is now running!')
+@bot.command()
+async def roll(ctx):
+    """Rolls a 6-sided die and sends the result."""
+    await ctx.send(str(random.randint(1, 6)))
 
-    @client.event
-    async def on_message(message):
-        # if a message is sent and the author is the bot(client.user), ignore it
-        if message.author == client.user:
-            return
-        
-        # get details about the message that was sent
-        username = str(message.author)
-        user_message = str(message.content)
-        channel = str(message.channel)
+@bot.command()
+async def help(ctx):
+    """DM the user a help message."""
+    await ctx.author.send('This is a help message.')
 
-        # print message details
-        print(f'{username} said: "{user_message}" ({channel})')
-
-
-        # if the first character of the user's message was a ?
-        if user_message[0] == '?':
-            # get the user message without the ?
-            user_message = user_message[1:]
-            # send a message privately
-            await send_message(message, user_message, is_private = True)
-        else:
-            # otherwise, if user message did not start with ?, send a message to the channel
-            await send_message(message, user_message, is_private = False)
-
-     
-    client.run(settings.DISCORD_API_SECRET)
+@bot.command()
+async def scheduleTest(ctx):
+    """DM the user a scheduled message."""
+    await ctx.send('You will receive a message in 10 seconds')
+    await asyncio.sleep(10)
+    await ctx.author.send('This is your scheduled message.')
+    
+# run the bot
+bot.run(settings.DISCORD_API_SECRET)
 
 
 
